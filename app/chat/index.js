5943861cddda9bfcaf5d77af9a0f58357b586850
VoNguyenThinh <thinhdbk11@gmail.com>
2021-04-25 00:27:33 +0700

    Implement dynamic chat board with mock data

--- app/chat/index.js
+++ app/chat/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const mockData = require("../../sample/data");
+const logData = require("../../sample/log")
 
 function getViewPath(file) {
     return path.join(__dirname, "../../views/" + file);
@@ -7,10 +8,10 @@ function getViewPath(file) {
 
 module.exports = {
     renderChat: (req, res) => {
-        const sessionUser = req.session.passport.user
-        console.log(mockData)
-        if(req.isAuthenticated())
-            res.render(getViewPath("chat"), { user: sessionUser, roomList: mockData});
+        if(req.isAuthenticated()) {
+            const sessionUser = req.session.passport.user;
+            res.render(getViewPath("chat"), { user: sessionUser, roomList: mockData, logData: logData });
+        }
         else res.redirect("/login");
     },
 
