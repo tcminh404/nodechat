58898c7877b0783bb2bd593a8b316cf49c231f30
VoNguyenThinh <thinhdbk11@gmail.com>
2021-04-24 23:38:25 +0700

    Add sample data and data rendering for chat panel, mock getlog api

--- app/chat/index.js
+++ app/chat/index.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const mockData = require("../../sample/data");
 
 function getViewPath(file) {
     return path.join(__dirname, "../../views/" + file);
@@ -7,8 +8,9 @@ function getViewPath(file) {
 module.exports = {
     renderChat: (req, res) => {
         const sessionUser = req.session.passport.user
+        console.log(mockData)
         if(req.isAuthenticated())
-            res.render(getViewPath("chat"), { user: sessionUser});
+            res.render(getViewPath("chat"), { user: sessionUser, roomList: mockData});
         else res.redirect("/login");
     },
 
