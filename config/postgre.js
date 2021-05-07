e55a585af700857b4227e3ee2ff5f7d591307433
tchanhtinh@gmail.com <tchanhtinh@gmail.com>
2021-05-07 07:46:10 +0700

    Change SSL env to fit heroku host

--- config/postgre.js
+++ config/postgre.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    ssl: process.env.DB_SSL === `true`,
+    ssl: { rejectUnauthorized: false },
     port: process.env.DB_PORT || 5432,
     max: 50,
     idleTimeoutMillis: 30000,
